f07cd707f31315e21fc1d1d2f32ea488075471e4
Anand <ch.anandsonu@gmail.com>
2017-02-22 15:25:20 +0530

    Initial commit

---
+++ app/src/main/java/com/bartronics/ams/AMSApp.java
@@ -0,0 +1,16 @@
+package com.bartronics.ams;
+
+import android.app.Application;
+
+/**
+ * Created by anand.chandaliya on 22-02-2017.
+ */
+
+public class AMSApp extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/bartronics/ams/data/AppDataManager.java
@@ -0,0 +1,8 @@
+package com.bartronics.ams.data;
+
+/**
+ * Created by anand.chandaliya on 22-02-2017.
+ */
+
+public class AppDataManager implements DataManager{
+}
